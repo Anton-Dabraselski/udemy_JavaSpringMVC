209f260cfbc3d6f138d7f4145717e5b9993fd3e9
Anton <dabraselski.anton@gmail.com>
2022-08-12 11:33:45 +0200

    @ModelAttribute

--- src/main/java/org/udemy/spring_mvc/controllers/PeopleController.java
+++ src/main/java/org/udemy/spring_mvc/controllers/PeopleController.java
@@ -3,10 +3,7 @@ package org.udemy.spring_mvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.udemy.spring_mvc.dao.PersonDAO;
 import org.udemy.spring_mvc.models.Person;
 
@@ -32,5 +29,16 @@ public class PeopleController {
         //Get 1 people by id from DAO -> view
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }

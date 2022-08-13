891ad8c24b855f9074b8bc2d5adeebb067c98a81
Anton <dabraselski.anton@gmail.com>
2022-08-14 00:15:29 +0200

    HTTP / CRUD / DAO / JDBC / JDBC Template

--- src/main/java/org/udemy/spring_mvc/dao/PersonDAO.java
+++ src/main/java/org/udemy/spring_mvc/dao/PersonDAO.java
@@ -1,6 +1,7 @@
 package org.udemy.spring_mvc.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -153,4 +154,58 @@ public class PersonDAO {
 //            throwables.printStackTrace();
 //        }
     }
+
+    ////////////////////////////////
+    ////Testing Multiple Update
+    ///////////////////////////////
+
+    public void testMultipleUpdate(){
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people){
+            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?,?,?)",
+                    person.getId(), person.getName(), person.getAge(), person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time: " + (after-before));
+    }
+
+    public void testBatchUpdate(){
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES(?,?,?,?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setInt(1, people.get(i).getId());
+                        preparedStatement.setString(2, people.get(i).getName());
+                        preparedStatement.setInt(3, people.get(i).getAge());
+                        preparedStatement.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after-before));
+    }
+
+    private List<Person> create1000People(){
+        List<Person> people =  new ArrayList<>();
+
+        for (int i = 6; i < 1000; i++) {
+            people.add(new Person(i, "Name"+i, 30, "test"+i+"mail.com"));
+        }
+        return people;
+    }
+
 }
